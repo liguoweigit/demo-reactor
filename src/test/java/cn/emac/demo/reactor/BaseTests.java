fb0e8c7685559b3acb390a6822bcf84296147368
yidao <liguowei@yongche.com>
2018-01-24 15:29:17 +0800

    add

--- src/test/java/cn/emac/demo/reactor/BaseTests.java
+++ src/test/java/cn/emac/demo/reactor/BaseTests.java
@@ -1,32 +1,35 @@
 package cn.emac.demo.reactor;
 
-import cn.emac.demo.reactor.repositories.ImperativeRestaurantRepository;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
+import cn.emac.demo.reactor.repositories.ImperativeRestaurantRepository;
 
 /**
  * @author Emac
  * @since 2017-06-04
  */
 public class BaseTests {
-
     public static final int CONCURRENT_SIZE = 100;
-    public static final int PACK_SIZE = 10_000;
-
-    private LocalDateTime start;
-
-    @BeforeEach
-    public void beforeEach() {
-        // start from scratch
-        ImperativeRestaurantRepository.INSTANCE.deleteAll();
-        start = LocalDateTime.now();
-    }
+    public static final int PACK_SIZE       = 10_000;
+    private LocalDateTime   start;
 
     @AfterEach
     public void afterEach() {
         System.out.println(Duration.between(start, LocalDateTime.now()).toMillis());
     }
+
+    @BeforeEach
+    public void beforeEach() {
+
+        // start from scratch
+        ImperativeRestaurantRepository.INSTANCE.deleteAll();
+        start = LocalDateTime.now();
+    }
 }
+
+
+//~ Formatted by Jindent --- http://www.jindent.com
--- src/test/java/cn/emac/demo/reactor/ThroughputTests.java
+++ src/test/java/cn/emac/demo/reactor/ThroughputTests.java
@@ -1,11 +1,5 @@
 package cn.emac.demo.reactor;
 
-import cn.emac.demo.reactor.domain.Restaurant;
-import cn.emac.demo.reactor.repositories.ImperativeRestaurantRepository;
-import cn.emac.demo.reactor.repositories.ReactiveRestaurantRepository;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
@@ -14,45 +8,62 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import cn.emac.demo.reactor.domain.Restaurant;
+import cn.emac.demo.reactor.repositories.ImperativeRestaurantRepository;
+import cn.emac.demo.reactor.repositories.ReactiveRestaurantRepository;
+
 /**
  * @author Emac
  * @since 2017-06-04
  */
 public class ThroughputTests extends BaseTests {
-
     private static List<Restaurant> load;
+
+    private void _runInParallel(int nThreads, Runnable task) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(nThreads);
+
+        for (int i = 0; i < nThreads; i++) {
+            executorService.submit(task);
+        }
+
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.MINUTES);
+    }
 
     @BeforeAll
     public static void beforeAll() {
-        // initialize load
-        load = IntStream.range(0, PACK_SIZE)
-                .mapToObj(i -> new Restaurant("hello" + i, "hello" + i, "hello" + i))
-                .collect(Collectors.toList());
+
+        /*
+         *  // initialize load
+         * load = IntStream.range(0, PACK_SIZE)
+         *        .mapToObj(i -> new Restaurant("hello" + i, "hello" + i, "hello" + i))
+         *        .collect(Collectors.toList());
+         */
     }
 
     @Test
     public void testImperative() throws InterruptedException {
-        _runInParallel(CONCURRENT_SIZE, () -> {
-            ImperativeRestaurantRepository.INSTANCE.insert(load);
-        });
-    }
-
-    private void _runInParallel(int nThreads, Runnable task) throws InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(nThreads);
-        for (int i = 0; i < nThreads; i++) {
-            executorService.submit(task);
-        }
-        executorService.shutdown();
-        executorService.awaitTermination(1, TimeUnit.MINUTES);
+        _runInParallel(CONCURRENT_SIZE,
+            () -> {
+                ImperativeRestaurantRepository.INSTANCE.insert(load);
+            });
     }
 
     @Test
     public void testReactive() throws InterruptedException {
         CountDownLatch latch = new CountDownLatch(CONCURRENT_SIZE);
+
         for (int i = 0; i < CONCURRENT_SIZE; i++) {
-            ReactiveRestaurantRepository.INSTANCE.insert(load).subscribe(s -> {
-            }, e -> latch.countDown(), latch::countDown);
+            ReactiveRestaurantRepository.INSTANCE.insert(load).subscribe(s -> {}
+            , e -> latch.countDown(), latch::countDown);
         }
+
         latch.await();
     }
 }
+
+
+//~ Formatted by Jindent --- http://www.jindent.com
